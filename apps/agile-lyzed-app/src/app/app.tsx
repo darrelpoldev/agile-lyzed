dc15f2ec9bff26daaee2030296cc87e56dd46181
Darrel Pol <poldarrel.dev@gmail.com>
2023-11-18 22:19:19 -0600

    adding chakra sample dashboard with the new react project

--- apps/agile-lyzed-app/src/app/app.tsx
+++ apps/agile-lyzed-app/src/app/app.tsx
@@ -1,8 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import NavBar from './nav-bar/nav-bar';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export function App() {
-  return <div>Hello, you've been AgileLyzed.</div>;
+  return (
+    <ChakraProvider>
+      <div className={styles['container']}>
+        <NavBar />
+        <h1>Hello, welcome to AgileLyzed!</h1>
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
