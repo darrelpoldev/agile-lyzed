dd38e3708055ee9c464b641b5c8b33ad3d3caa79
Darrel Pol <poldarrel.dev@gmail.com>
2023-11-18 20:32:05 -0600

    setting up vscode prettier format on save using prettier

--- apps/agile-lyzed-app/src/app/app.tsx
+++ apps/agile-lyzed-app/src/app/app.tsx
@@ -1,14 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <div>
-      Hello, you've been AgileLyzed.
-    </div>
-  );
+  return <div>Hello, you've been AgileLyzed.</div>;
 }
 
 export default App;
