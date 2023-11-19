49e8f999463e26b2d42067462a0ea951f8f022cc
Darrel Pol <poldarrel.dev@gmail.com>
2023-11-18 23:12:22 -0600

    adding footer place holder

--- apps/agile-lyzed-app/src/app/app.tsx
+++ apps/agile-lyzed-app/src/app/app.tsx
@@ -1,15 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import NavBar from './nav-bar/nav-bar';
-import { ChakraProvider } from '@chakra-ui/react';
+import { Box, ChakraProvider } from '@chakra-ui/react';
 
 export function App() {
   return (
     <ChakraProvider>
-      <div className={styles['container']}>
-        <NavBar />
-        <h1>Hello, welcome to AgileLyzed!</h1>
-      </div>
+      <NavBar />
+      <Box>Set footer here</Box>
     </ChakraProvider>
   );
 }
--- apps/agile-lyzed-app/src/app/nav-bar/nav-bar.tsx
+++ apps/agile-lyzed-app/src/app/nav-bar/nav-bar.tsx
@@ -25,6 +25,10 @@ const routes: NavBarLinks[] = [
     navigationRoute: '/tools',
   },
   {
+    displayText: 'Blog',
+    navigationRoute: '/blog',
+  },
+  {
     displayText: 'About Me',
     navigationRoute: '/about-me',
   },
@@ -60,55 +64,67 @@ export function NavBar(props: NavBarProps) {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-        <IconButton
-          size={'md'}
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          aria-label={'Open Menu'}
-          display={{ md: 'none' }}
-          onClick={isOpen ? onClose : onOpen}
-        />
-        <HStack spacing={8} alignItems={'center'}>
-          <Box>DarrelPol</Box>
-          <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
-            {routes.map((route) => (
-              <NavLink key={route.navigationRoute}>{route.displayText}</NavLink>
-            ))}
-          </HStack>
-        </HStack>
-        <Flex alignItems={'center'}>
-          <Menu>
-            <MenuButton
-              as={Button}
-              rounded={'full'}
-              variant={'link'}
-              cursor={'pointer'}
-              minW={0}
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Box>DarrelPol</Box>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}
             >
-              <Avatar
-                size={'sm'}
-                src={
-                  'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                }
-              />
-            </MenuButton>
-            <MenuList>
-              <MenuItem>Settings</MenuItem>
-            </MenuList>
-          </Menu>
+              {routes.map((route) => (
+                <NavLink key={route.navigationRoute}>
+                  {route.displayText}
+                </NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={'center'}>
+            <Menu>
+              <MenuButton
+                as={Button}
+                rounded={'full'}
+                variant={'link'}
+                cursor={'pointer'}
+                minW={0}
+              >
+                <Avatar
+                  size={'sm'}
+                  src={
+                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                  }
+                />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Join</MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
         </Flex>
-      </Flex>
-      {isOpen ? (
-        <Box pb={4} display={{ md: 'none' }}>
-          <Stack as={'nav'} spacing={4}>
-            {routes.map((route) => (
-              <NavLink key={route.navigationRoute}>{route.displayText}</NavLink>
-            ))}
-          </Stack>
-        </Box>
-      ) : null}
-    </Box>
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {routes.map((route) => (
+                <NavLink key={route.navigationRoute}>
+                  {route.displayText}
+                </NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+
+      <Box p={4}>Main Content Here</Box>
+    </>
   );
 }
 
