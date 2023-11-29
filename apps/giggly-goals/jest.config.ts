f69ee4d6ac9af9c079a0246009bf8af2f0f5e3b2
Darrel Pol <poldarrel.dev@gmail.com>
2023-11-28 20:23:01 -0600

    adding giggly-goals project

---
+++ apps/giggly-goals/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'giggly-goals',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/giggly-goals',
+};
