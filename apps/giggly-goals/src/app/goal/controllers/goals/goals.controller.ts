23475f65006c0651c20210e6fba481920c2ff1c2
Darrel Pol <poldarrel.dev@gmail.com>
2023-12-01 18:08:39 -0600

    adding POST endpoint to create goal

--- apps/giggly-goals/src/app/goal/controllers/goals/goals.controller.ts
+++ apps/giggly-goals/src/app/goal/controllers/goals/goals.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { GoalUseCaseService } from '../../use-cases';
 import { Goal } from '../../domain';
+import { CreateGoalDTO } from '../../use-cases/goal/dtos/goal.dtos';
 
 @Controller('goals')
 export class GoalsController {
@@ -9,5 +10,11 @@ export class GoalsController {
   @Get()
   async findAll(): Promise<Goal[]> {
     return this.goalUsecase.getAllGoals();
+  }
+
+  @Post()
+  @HttpCode(201)
+  async create(@Body() createGoalDTO: CreateGoalDTO): Promise<Goal> {
+    return this.goalUsecase.createGoal(createGoalDTO);
   }
 }
--- apps/giggly-goals/src/app/goal/use-cases/goal/goal-use-case.service.ts
+++ apps/giggly-goals/src/app/goal/use-cases/goal/goal-use-case.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Goal } from '../../domain/entities';
 import { IDataProvider } from '../../domain/abstracts/data-provider.abstract';
+import { CreateGoalDTO } from './dtos/goal.dtos';
 
 @Injectable()
 export class GoalUseCaseService {
@@ -9,12 +10,17 @@ export class GoalUseCaseService {
     return this.dataProvider.goals.getAll();
   }
 
-  getGoalById(): Promise<Goal> {
-    throw new Error('Method not implemented');
+  getGoalById(id: string): Promise<Goal> {
+    return this.dataProvider.goals.getById(id);
   }
 
-  createGoal(): Promise<Goal> {
-    throw new Error('Method not implemented');
+  async createGoal(createGoalDTO: CreateGoalDTO): Promise<Goal> {
+    const newGoal = new Goal();
+    newGoal.title = createGoalDTO.title;
+    newGoal.description = createGoalDTO.description;
+
+    const createdGoal = await this.dataProvider.goals.create(newGoal);
+    return createdGoal;
   }
 
   updateGoal(): Promise<Goal> {
