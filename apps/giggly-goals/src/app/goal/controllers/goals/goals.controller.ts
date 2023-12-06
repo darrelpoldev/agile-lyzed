0ecdd27f8c54736bc63ebad38c8c053a7b5a14be
Darrel Pol <poldarrel.dev@gmail.com>
2023-12-05 19:48:59 -0600

    adding swagger configuration

--- apps/giggly-goals/src/app/goal/controllers/goals/goals.controller.ts
+++ apps/giggly-goals/src/app/goal/controllers/goals/goals.controller.ts
@@ -14,27 +14,49 @@ import {
   CreateGoalDTO,
   UpdateGoalDTO,
 } from '../../use-cases/goal/dtos/goal.dtos';
+import { ApiTags, ApiBody, ApiOperation } from '@nestjs/swagger';
 
 @Controller('goals')
+@ApiTags('Goals')
 export class GoalsController {
   constructor(private goalUsecase: GoalUseCaseService) {}
 
+  @ApiOperation({
+    description: 'Get all goals.',
+  })
   @Get()
   async findAll(): Promise<Goal[]> {
     return this.goalUsecase.getAllGoals();
   }
 
+  @ApiOperation({
+    description: 'Get goals by Id.',
+  })
   @Get(':id')
   async findById(@Param('id') goalId: string): Promise<Goal> {
     return this.goalUsecase.getGoalById(goalId);
   }
 
+  @ApiOperation({
+    description: 'Create goal by providing the details',
+  })
+  @ApiBody({
+    type: CreateGoalDTO,
+    description: 'Payload to create goal',
+  })
   @Post()
   @HttpCode(201)
   async create(@Body() createGoalDTO: CreateGoalDTO): Promise<Goal> {
     return this.goalUsecase.createGoal(createGoalDTO);
   }
 
+  @ApiOperation({
+    description: 'Update goal details.',
+  })
+  @ApiBody({
+    type: UpdateGoalDTO,
+    description: 'Payload to update goal.',
+  })
   @Put(':id')
   async update(
     @Param('id') goalId: string,
@@ -43,6 +65,9 @@ export class GoalsController {
     return this.goalUsecase.updateGoal(goalId, updateGoalDTO);
   }
 
+  @ApiOperation({
+    description: 'Delete goal by Id.',
+  })
   @Delete(':id')
   async delete(@Param('id') goalId: string) {
     return this.goalUsecase.deleteGoal(goalId);
--- apps/giggly-goals/src/app/goal/use-cases/goal/dtos/goal.dtos.ts
+++ apps/giggly-goals/src/app/goal/use-cases/goal/dtos/goal.dtos.ts
@@ -1,11 +1,20 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateGoalDTO {
+  @ApiProperty({
+    example: 'Buy a new house',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    example: 'This is going to be my first asset',
+    required: false,
+  })
   @IsString()
   @IsNotEmpty()
   description: string;
